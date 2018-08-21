8e666c09167d402a18adfa92c4e41004f2fbb161
Spuniu <marcin.spurgiasz@capgemini.com>
2018-08-21 12:47:32 +0200

    New path to static files - attempt 2

--- src/app.js
+++ src/app.js
@@ -7,10 +7,18 @@ module.exports = async function appFactory() {
   const app = express();
   const routes = await routesFactory();
 
-    app.use(express.static(path.join(__dirname, "/client/build/static/css")));
-    app.use(express.static(path.join(__dirname, "/client/build/static/js")));
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(routes);
+    app.get('/api/hello', (req, res) => {
+        res.send({express: 'Hello From Express'});
+    });
+
+    if (process.env.NODE_ENV === 'production') {
+        app.use(express.static(path.join(__dirname, "/client/build")));
+        app.get('*', function (req, res) {
+            res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+        });
+    }
+    // app.use(bodyParser.urlencoded({ extended: true }));
+    // app.use(routes);
 
   return app;
 };
