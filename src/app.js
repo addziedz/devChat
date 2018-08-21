408803fbd4f7b0a5d9813ab4a50cc65a83a07586
qxp4728 <adam.dziedzic@partner.bmw.de>
2018-08-21 11:26:49 +0200

    master: fix path issue 5

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ module.exports = async function appFactory() {
   const app = express();
   const routes = await routesFactory();
 
-  app.use(express.static(path.join(__dirname, "public")));
+  app.use(express.static(path.join(__dirname, "/client/build")));
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(routes);
 
