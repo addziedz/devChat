b84324d37ddb36dcb31b8b3e6775965a29f71b24
Edi-soN <adamdziedzic87@gmail.com>
2018-08-28 19:53:27 +0200

    master: testing connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,12 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Chat from "./containers/Chat/Chat";
-import './App.css';
+import "./App.css";
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {};
-  }
   render() {
     return (
         <div className="App">
-        <Chat user="user1"/>
+          <Chat user="user1"/>
       </div>
     );
   }
--- src/server.js
+++ src/server.js
@@ -7,41 +7,40 @@ const userService = require("./userService");
   const server = app.listen(config.port, () => {
     console.log(`App listening on port ${config.port}!`);
   });
-    const io = require("socket.io")(server);
-    const us = userService();
+  const io = require("socket.io")(server);
+  const us = userService();
 
-    io.on("connection", socket => {
-    console.log("User connected!!!");
+  io.on("connection", socket => {
+    console.log("User connected!!!" + socket.id);
 
-        socket.on("join", username => {
-            us.addUser({
-                id: socket.id,
-                username
-            });
-            io.emit("updateUsers", {
-                users: us.getAllUsers()
-            });
-        });
+    socket.on("join", username => {
+      us.addUser({
+        id: socket.id,
+        username
+      });
+      io.emit("updateUsers", {
+        users: us.getAllUsers()
+      });
+    });
 
-        socket.on("disconnect", () => {
-            us.removeUser(socket.id);
-            socket.broadcast.emit("updateUsers", {
-                users: us.getAllUsers()
-            });
-        });
+    socket.on("disconnect", () => {
+      us.removeUser(socket.id);
+      socket.broadcast.emit("updateUsers", {
+        users: us.getAllUsers()
+      });
+    });
 
-        socket.on("message", message => {
-            // const {username} = us.getUserById(socket.id);
-            socket.broadcast.emit("message", {
-                text: message.text,
-                from: message.from
-            });
-            // console.log(message.text + " " + message.from);
-        });
+    socket.on("message", message => {
+      // const {username} = us.getUserById(socket.id);
+      socket.broadcast.emit("message", {
+        text: message.text,
+        from: message.from
+      });
+    });
 
-        socket.on('typing', username => {
-            console.log(username);
-            socket.broadcast.emit('typing', username);
-        });
+    socket.on("typing", username => {
+      console.log(username);
+      socket.broadcast.emit("typing", username);
+    });
   });
 })();
